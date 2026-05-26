2022830a2fb099b1c20d10acab24381583149c6f
Martin Buergi <martin.buergi@gmail.com>
2026-05-26 20:20:33 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -168,51 +168,7 @@ async function loadLazy(doc) {
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
-
-  ...
-
-/**
- * Loads the page in eager, lazy, and delayed phases.
- */
-export async function loadPage() {
-
-...
-
-/**
- * Loads everything that doesn't need to be delayed.
- * @param {Element} doc The container element
- */
-async function loadLazy(doc) {
-
-  ...
-
-  const loadQuickEdit = async (...args) => {
-    // eslint-disable-next-line import/no-cycle
-    const { default: initQuickEdit } = await import('../tools/quick-edit/quick-edit.js');
-    initQuickEdit(...args);
-  };
-
-  const addSidekickListeners = (sk) => {
-    sk.addEventListener('custom:quick-edit', loadQuickEdit);
-  };
-
-  const sk = document.querySelector('aem-sidekick');
-  if (sk) {
-    addSidekickListeners(sk);
-  } else {
-    // wait for sidekick to be loaded
-    document.addEventListener('sidekick-ready', () => {
-    // sidekick now loaded
-      addSidekickListeners(document.querySelector('aem-sidekick'));
-    }, { once: true });
-  }
 }
-
-(() => {
-  const hasQE = new URL(window.location.href).searchParams.has('quick-edit');
-  if (hasQE) import('../tools/quick-edit/quick-edit.js').then((mod) => mod.default());
-})();
-
 
 /**
  * Loads everything that happens a lot later,
@@ -224,7 +180,7 @@ function loadDelayed() {
   // load anything that can be postponed to the latest here
 }
 
-export async function loadPage() {
+async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
   loadDelayed();
